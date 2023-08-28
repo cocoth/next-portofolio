f8b952af2163c0705c13d32b912ca07667a92a81
anonymousid <anonymousid0000000@gmail.com>
2023-08-28 10:26:02 +0700

    2nd push

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import '@/styles/globals.css'
 import { ThemeProvider } from 'next-themes';
+import { Analytics } from '@vercel/analytics/react';
+
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider enableSystem={true} attribute='class'> 
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
